784135776b5610aa06eca572a75b432ccd76ee4c
Kazuki Imamura <kaizumaki@gmail.com>
2022-04-15 02:10:42 +0900

    [WIP]画像アップロードを実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,56 @@
 <template>
-  <Tutorial />
+  <div>
+    <div ref="result">
+      <div class="manga-container">
+        <img src="/test.png" alt="" />
+        <p>{{ text }}</p>
+      </div>
+    </div>
+    <input v-model="text" type="text" />
+    <button type="button" @click="uploadImage">upload</button>
+    <div v-if="downloadURL">
+      <a :href="downloadURL" target="_blank">{{ downloadURL }}</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import html2canvas from 'html2canvas'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  data() {
+    return {
+      text: '',
+      downloadURL: '',
+    }
+  },
+  methods: {
+    uploadImage() {
+      const storageRef = this.$fire.storage.ref()
+      const randomString = this.getRandomString(8)
+      const now = Date.now()
+      html2canvas(this.$refs.result as HTMLElement).then((canvas) => {
+        canvas.toBlob((blob) => {
+          storageRef
+            .child(`${randomString}${now}.png`)
+            .put(blob!)
+            .then((snapshot) => {
+              snapshot.ref.getDownloadURL().then((downloadURL) => {
+                this.downloadURL = downloadURL
+              })
+            })
+        }, 'image/png')
+      })
+    },
+    getRandomString(length) {
+      const source = 'abcdefghijklmnopqrstuvwxyz'
+      let result = ''
+      for (let i = 0; i < length; i++) {
+        result += source[Math.floor(Math.random() * source.length)]
+      }
+      return result
+    },
+  },
 })
 </script>

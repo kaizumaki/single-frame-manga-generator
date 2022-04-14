f2815793216de984964a7e5101bb89e2d82a858e
Kazuki Imamura <kaizumaki@gmail.com>
2022-04-15 01:15:12 +0900

    firebase設定を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,8 +32,22 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/firebase'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  firebase: {
+    config: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+    },
+    services: {
+      storage: true,
+    },
+  },
 }
